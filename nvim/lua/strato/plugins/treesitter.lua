ca8fa501e22face4b5d2679633603dfdf435e2e8
SantiMartinezG <san_mtzg@outlook.es>
2026-05-25 15:55:17 -0600

    Migrate treesitter and textobjects to main branch

--- nvim/lua/strato/plugins/treesitter.lua
+++ nvim/lua/strato/plugins/treesitter.lua
@@ -1,91 +1,82 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
 	lazy = false,
 	build = ":TSUpdate",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				-- Core
+		local ts = require("nvim-treesitter")
+
+		ts.setup()
+
+		-- parsers
+		ts.install({
+			"vim",
+			"vimdoc",
+			"lua",
+			"query",
+			"regex",
+			"bash",
+			"fish",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			"json",
+			"yaml",
+			"go",
+			"gomod",
+			"gosum",
+			"gowork",
+			"python",
+			"markdown",
+			"markdown_inline",
+			"dockerfile",
+			"toml",
+			"gitcommit",
+			"gitignore",
+			"git_config",
+			"git_rebase",
+			"diff",
+		})
+
+		-- open all folds by default
+		vim.opt.foldlevelstart = 99
+
+		-- enable highlight + indent + folds per filetype
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
 				"vim",
-				"vimdoc",
 				"lua",
-				"query",
-				"regex",
-				-- Shell
 				"bash",
 				"fish",
-				-- Web
 				"html",
 				"css",
 				"javascript",
 				"typescript",
-				"tsx",
+				"typescriptreact",
 				"json",
 				"yaml",
-				-- Backend
 				"go",
 				"gomod",
 				"gosum",
 				"gowork",
 				"python",
-				-- Markup / config
 				"markdown",
-				"markdown_inline",
 				"dockerfile",
 				"toml",
-				-- Git
 				"gitcommit",
 				"gitignore",
-				"git_config",
-				"git_rebase",
+				"gitconfig",
+				"gitrebase",
 				"diff",
 			},
-			auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<leader>v",
-					node_incremental = "<leader>v",
-					scope_incremental = false,
-					node_decremental = "<BS>",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-				},
-			},
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldmethod = "expr"
+			end,
 		})
 	end,
 }
